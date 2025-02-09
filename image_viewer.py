ee297ff2f7cb4b1528c28781c3394963d313e2f5
aliihsan <alihsan_kerem_kahya@hotmail.com>
2025-02-09 23:32:48 +0200

    completed image viewer

--- image_viewer.py
+++ image_viewer.py
@@ -2,25 +2,46 @@ from tkinter import *
 from PIL import ImageTk, Image
 
 
-global screen
-screen = 0
 
-def next(number):
-    global screen
-    screen = number + 1
-def previous(number):
-    global screen
-    screen = number - 1
+
+def next(image_number):
+    global my_label
+    global button_next
+    global button_previous
+
+    my_label.grid_forget()
+    my_label = Label(image=images[image_number - 1])
+    button_next = Button(root, text=">>", command=lambda: next(image_number + 1))
+    button_previous = Button(root, text="<<", command=lambda: previous(image_number - 1))
+
+    if image_number == 5:
+        button_next = Button(root, text=">>", state=DISABLED)
+
+    my_label.grid(row=0, column=0, columnspan=3)
+    button_next.grid(row=1, column=2)
+    button_previous.grid(row=1, column=0)
+
+
+def previous(image_number):
+    global my_label
+    global button_next
+    global button_previous
+
+    my_label.grid_forget()
+    my_label = Label(image=images[image_number - 1])
+    button_next = Button(root, text=">>", command=lambda: next(image_number + 1))
+    button_previous = Button(root, text="<<", command=lambda: previous(image_number - 1))
+
+    if image_number == 1:
+        button_previous = Button(root, text="<<", state=DISABLED)
+
+    my_label.grid(row=0, column=0, columnspan=3)
+    button_next.grid(row=1, column=2)
+    button_previous.grid(row=1, column=0)
 
 root = Tk()
 root.title("Usages of Images")
 root.iconbitmap("list.ico")
-
-button_next = Button(root, text=">>", command=lambda: next(screen))
-button_next.pack()
-
-button_previous = Button(root, text="<<", command=lambda: previous(screen))
-button_previous.pack()
 
 my_image1 = ImageTk.PhotoImage(Image.open("Cat_Photos/cat.jpeg"))
 my_image2 = ImageTk.PhotoImage(Image.open("Cat_Photos/cat2.jpg"))
@@ -30,15 +51,16 @@ my_image5 = ImageTk.PhotoImage(Image.open("Cat_Photos/cat5.png"))
 images = [my_image1, my_image2, my_image3, my_image4, my_image5]
 
 
-my_label = Label(image=images[screen])
-my_label.pack()
-
-
-
-
+my_label = Label(image=images[0])
+my_label.grid(row=0, column=0, columnspan=3)
 
 button_quit = Button(root, text="Exit program", command=root.quit)
-button_quit.pack()
+button_quit.grid(row=1, column=1)
+button_next = Button(root, text=">>", command=lambda: next(2))
+button_next.grid(row=1, column=2)
+
+button_previous = Button(root, text="<<", command=previous)
+button_previous.grid(row=1, column=0)
 
 
 
